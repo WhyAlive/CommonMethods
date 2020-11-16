3a8d2b82a814595f64e0e754660e42bdd904efb8
supeng <974204136@qq.com>
2020-11-16 14:00:38 +0800

    获取url ？后面的参数

---
+++ getQueryString.js
@@ -0,0 +1,11 @@
+function getLocatoin() {
+  let url = window.location.search.substr(1);
+  var obj = {};
+  // for (let i = 0; i < url.split('&').length; i++) {
+  //   obj[url.split('&')[i].split('=')[0]] = url.split('&')[i].split('=')[1]
+  // }
+  url
+    .split("&")
+    .forEach((item) => (obj[item.split("=")[0]] = item.split("=")[1]));
+  return obj;
+}
